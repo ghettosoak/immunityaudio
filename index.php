ddc589603aed59cf3a352b95144d8bc21f46772e
Mike <ghettosoak@gmail.com>
2014-08-16 15:52:14 +0200

    First Commit!
    Here's everything before we apply the boilerplate: the teaser page, and some design work. Nice!

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<!--[if lt IE 7]>  <html class="ie ie8 ie7 ie6" lang="en"> <![endif]-->
+<!--[if IE 7]>     <html class="ie ie8 ie7" lang="en"> <![endif]-->
+<!--[if IE 8]>     <html class="ie ie8" lang="en"> <![endif]-->
+<!--[if IE]>       <html class="ie" lang="en"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>IMMUNITY AUDIO</title>
+    <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="css/master.css" />
+    <link rel="shortcut icon" href="img/favicon.ico" />
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png" />
+
+</head>
+
+<body>
+	<div id="master">
+		<div class="logo"></div>
+		<div class="links">
+			<p>
+				<a href="https://www.youtube.com/user/ImmunityAudio" target="_blank">youtube</a>
+				<a href="http://immunityaudio.bandcamp.com/" target="_blank">bandcamp</a>
+				<a href="http://immunityaudio.tumblr.com/" target="_blank">blog</a>
+			</p>
+			<p class="comingsoon">SITE COMING SOON</p>
+		</div>
+	</div>
+</body>
+</html>
